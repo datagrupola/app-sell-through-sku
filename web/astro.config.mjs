448d14f1f672213e5a0842db8725d334071e563d
TarLomin <josueescobedojnz@gmail.com>
2026-07-06 17:50:08 -0600

    Deploy Astro app with GitHub Pages workflow

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,5 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://datagrupola.github.io',
+  base: '/app-sell-through-sku',
+});
